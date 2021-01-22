9760afdf02e455a566bf3752278e38a8c44f1e99
Alan Barton <abarton201@gmail.com>
2021-01-21 20:54:43 -0500

    editing

--- src/views/ReviewEdit.vue
+++ src/views/ReviewEdit.vue
@@ -7,11 +7,6 @@
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
 
-      <!-- <h1>{{ professor.name }}</h1> -->
-      <div class="form-group">
-        <label>Professor:</label>
-        <input type="text" class="form-control" v-model="reviews.professors_id" />
-      </div>
       <div class="form-group">
         <label>rating:</label>
         <input type="text" class="form-control" v-model="reviews.rating" />
