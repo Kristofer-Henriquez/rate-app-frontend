e95cdab86b7c55fb89960e1725c573ccb5bfc879
Alan Barton <abarton201@gmail.com>
2021-01-24 22:14:20 -0500

    style professor create

--- src/views/ProfessorCreate.vue
+++ src/views/ProfessorCreate.vue
@@ -1,22 +1,65 @@
 <template>
   <div class="Professors-Create">
     <h2>New Professor</h2>
-    <form v-on:submit.prevent="createProfessor()">
-      <ul>
-        <li v-for="error in errors">{{ error }}</li>
-      </ul>
-      <div>
-        Name:
-        <input type="text" v-model="newProfessorName" />
-      </div>
-      Title:
-      <input type="text" v-model="newProfessorTitle" />
-      school:
-      <input type="text" v-model="newProfessorSchool" />
-      department:
-      <input type="text" v-model="newProfessorDepartment" />
-      <input type="submit" value="Create" />
-    </form>
+
+    <div style="width: 75rem; height: auto; margin: auto; border-color: black; border-radius: 15px; border-width: 5px">
+      <b-card>
+        <form v-on:submit.prevent="createProfessor()">
+          <ul>
+            <li v-for="error in errors">{{ error }}</li>
+          </ul>
+          <b-row class="my-1">
+            <b-col sm="2">
+              <label for="input-default">Professor Name:</label>
+            </b-col>
+            <b-col sm="10">
+              <b-form-input
+                id="input-default"
+                placeholder="Enter Professor's name"
+                v-model="newProfessorName"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <b-row class="my-1">
+            <b-col sm="2">
+              <label for="input-default">Title:</label>
+            </b-col>
+            <b-col sm="10">
+              <b-form-input
+                id="input-default"
+                placeholder="Enter Professor's title"
+                v-model="newProfessorTitle"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <b-row class="my-1">
+            <b-col sm="2">
+              <label for="input-default">School:</label>
+            </b-col>
+            <b-col sm="10">
+              <b-form-input
+                id="input-default"
+                placeholder="Enter Professor's School"
+                v-model="newProfessorSchool"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <b-row class="my-1">
+            <b-col sm="2">
+              <label for="input-default">Department:</label>
+            </b-col>
+            <b-col sm="10">
+              <b-form-input
+                id="input-default"
+                placeholder="Enter Professor's Department"
+                v-model="newProfessorDepartment"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <input type="submit" value="Create" style="text-align: left;" />
+        </form>
+      </b-card>
+    </div>
   </div>
 </template>
 
