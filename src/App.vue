501869591e827cf02dda33e7e636e63c204c2bc2
Kristofer <kristoferhenriquez@gmail.com>
2021-01-24 20:09:35 -0600

    overall style to index page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
+    <div id="logo"> 
+	<img src="./Images/LogoMakr-5k2t6X.png"> 
+</div> 
     <div id="nav">
-      <router-link to="/professors">Home</router-link>
-      |
-      <router-link to="/professorsNew">New Professor</router-link>
+      <router-link to="/professors" style="color: #f8ae0ff3; font-size:46px">All Professors</router-link>
+      <div>
+      </div>
+      <router-link to="/professorsNew" style="color: #f8ae0ff3; font-size:46px">New Professor</router-link>
     </div>
     <router-view />
   </div>
@@ -15,11 +19,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f8ae0ff3;
 }
 
 #nav {
   padding: 30px;
+}
+
+body {
+ background-image: url("./Images/cqSJ92.jpg");
+ background-size: auto;
 }
 
 #nav a {
--- src/views/ProfessorsIndex.vue
+++ src/views/ProfessorsIndex.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="Professors">
-    <h1>This is the Index action for professors</h1>
+    <h1>The tables have turned!</h1>
     <div class="row">
-      <div class="col-sm-6" v-for="professor in professors">
-        <div class="card">
-          <div class="card-body">
+      <div class="col-sm-3" v-for="professor in professors">
+        <br>
+        <div class="card" style="width: 20rem; height: auto; margin: 25px; border-color: orange; border-radius: 15px; border-width: 5px">
+          <div class="card-body" style="width: auto; height: auto; color: #080601f3;">
             <h2>{{ professor.name }}</h2>
             <h3> {{ professor.avg | formatNumber}} </h3>
             <p>{{ professor.school }}</p>
